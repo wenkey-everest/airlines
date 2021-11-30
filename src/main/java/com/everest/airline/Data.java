e6d1ddd981f3fef36286ceacc869cb15298ba9ec
Ravi Bolla <ravi.bolla@everest.engineering>
2021-11-30 10:35:30 +0530

    Add static data

---
+++ src/main/java/com/everest/airline/Data.java
@@ -0,0 +1,13 @@
+package com.everest.airline;
+
+import java.util.List;
+
+public class Data {
+    static List<Flight> flights = List.of(
+            new Flight(1001, "Hyderabad", "Bangalore"),
+            new Flight(1002, "Bangalore", "Hyderabad"),
+            new Flight(1003, "Goa", "Bangalore"),
+            new Flight(1004, "Bangalore", "Goa"),
+            new Flight(1005, "Bangalore", "Hyderabad"),
+            new Flight(1006, "Hyderabad", "Bangalore"));
+}
