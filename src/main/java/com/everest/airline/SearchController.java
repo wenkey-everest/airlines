11fb4d4b13437685a4c0f654f9c3e55799af3724
Ravi Bolla <ravi.bolla@everest.engineering>
2021-11-30 10:37:34 +0530

    Add search results page

--- src/main/java/com/everest/airline/SearchController.java
+++ src/main/java/com/everest/airline/SearchController.java
@@ -1,6 +1,7 @@
 package com.everest.airline;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,4 +12,9 @@ public class SearchController {
         return "home";
     }
 
+    @RequestMapping(value = "/search")
+    public String search(String from, String to, Model model) {
+        model.addAttribute("flights", Data.flights);
+        return "search";
+    }
 }
