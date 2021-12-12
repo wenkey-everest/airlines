5908f6431078a15bd450213081779fb5d48aa237
wenkey <muni.v.g@everest.engineering>
2021-12-12 15:09:41 +0530

    improved validation

--- src/main/java/com/everest/airline/services/SearchService.java
+++ src/main/java/com/everest/airline/services/SearchService.java
@@ -30,7 +30,7 @@ public class SearchService {
                     try {
                         bufferedReader = new BufferedReader(new FileReader(DataParser.multiFileReader()[i]));
                         String[] strings = bufferedReader.readLine().split(",", -1);
-                        if ((strings[1].equalsIgnoreCase(from)) && (strings[2].equalsIgnoreCase(to)))
+                            if ((strings[1].equalsIgnoreCase(from)) && (strings[2].equalsIgnoreCase(to)) && (strings[3].equalsIgnoreCase(departureDate)))
                             flightList.add(new Flight(Long.parseLong(strings[0]), strings[1], strings[2], LocalDate.parse(strings[3]), LocalTime.parse(strings[4]), LocalDate.parse(strings[5]), LocalTime.parse(strings[6]), Integer.parseInt(strings[7])));
                     } catch (IOException e) {
                         e.printStackTrace();
