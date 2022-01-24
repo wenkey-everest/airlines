4525c7cb4f3178169babeb30ec4765de08690071
wenkey <muni.v.g@everest.engineering>
2022-01-24 21:25:50 +0530

    removed unwanted local variables

--- src/main/java/com/everest/airline/services/BookTicketService.java
+++ src/main/java/com/everest/airline/services/BookTicketService.java
@@ -13,10 +13,9 @@ import java.util.stream.Collectors;
 @Component
 public class BookTicketService {
 
-
     public List<Flight> bookTicket(String noOfPass, Long number, String flightClass) {
-        NamedParameterJdbcTemplate jdbcTemplate = new DbConfig().namedParameterJdbcTemplate();
-        return jdbcTemplate.query("select * from flights", new FlightRowMapper())
+        return new DbConfig().namedParameterJdbcTemplate()
+                .query("select * from flights", new FlightRowMapper())
                 .stream().filter(flight -> flight.getNumber() == number)
                 .peek(flight -> new FilterClass(noOfPass,flight,flightClass).testExecute())
                 .collect(Collectors.toList());
